325e4dbf45be95cb52366b92e3e6310633b0972e
Harsh <harshpachani@gmail.com>
2023-07-25 19:21:13 +0530

    Connected the delete endpoint to the frontend

--- routes/task.js
+++ routes/task.js
@@ -1,15 +1,20 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
 import { deleteTask, getMyTask, newTask, updateTask } from "../controllers/task.js"; 
+import  methodOverride from "method-override";
 
 const router = express.Router();
+
+router.use(methodOverride("_method"));
 
 router.post("/new", isAuthenticated, newTask);
 router.get("/tasks", isAuthenticated, getMyTask);
 
 router
-    .route(":/id")
-    .put(isAuthenticated, updateTask)
-    .delete(isAuthenticated, deleteTask);
+    .route("/:id")
+    .put(isAuthenticated, updateTask);
+    // .delete(isAuthenticated, deleteTask);
+
+router.delete('/:id', isAuthenticated, deleteTask);
     
 export default router;
